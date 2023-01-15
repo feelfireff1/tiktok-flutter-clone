9e5e5f482808ccf1a934db470ae930ef32b535cc
Soojin Ro <sugarpoint33@gmail.com>
2023-01-14 23:44:21 -0500

    #4.2 AuthButton

--- tiktok_clone/lib/features/authentication/sign_up_screen.dart
+++ tiktok_clone/lib/features/authentication/sign_up_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/constants.dart';
 import 'package:tiktok_clone/features/authentication/login_screen.dart';
 import 'package:tiktok_clone/features/authentication/widgets/auth_button.dart';
@@ -37,8 +38,15 @@ class SignUpScreen extends StatelessWidget {
               textAlign: TextAlign.center,
             ),
             Gaps.v40,
-            AuthButton(text: 'Use phone or email'),
-            AuthButton(text: 'Continue with Apple'),
+            AuthButton(
+              icon: FaIcon(FontAwesomeIcons.user),
+              text: 'Use email & password',
+            ),
+            Gaps.v16,
+            AuthButton(
+              icon: FaIcon(FontAwesomeIcons.apple),
+              text: 'Continue with Apple',
+            ),
           ]),
         ),
       ),
